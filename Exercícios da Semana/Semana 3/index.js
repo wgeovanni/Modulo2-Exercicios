4072384a02367731486700a55b6ac82315a3bee7
wgeovanni <willyangeo@gmail.com>
2023-07-31 19:35:06 -0300

    Exericio 3

--- Exercícios da Semana/Semana 3/index.js
+++ Exercícios da Semana/Semana 3/index.js
@@ -2,5 +2,14 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+
+    switch (req.method) {
+        case "GET":
+            // aqui vai a lógica do GET - exercício 5
+            break
+        case "POST":
+            // aqui vai a lógica do POST - exercício 7
+            break
+    }
 })
 server.listen(3000);
