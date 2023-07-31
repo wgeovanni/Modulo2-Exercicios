95ccaf47351b929a9b0197e93de2cfe5168f02bb
wgeovanni <willyangeo@gmail.com>
2023-07-31 19:43:07 -0300

    Exercicio 4 - Função para listagem de produtos

--- Exercícios da Semana/Semana 3/index.js
+++ Exercícios da Semana/Semana 3/index.js
@@ -1,6 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 
+function ListarProdutos() {
+    try {
+        const dados = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
+        console.log(dados)
+        return JSON.stringify(dados.produtos)
+    } catch (erro) {
+        return "Erro ao executar"
+    }
+}
+
 const server = http.createServer((req, res) => {
 
     switch (req.method) {
