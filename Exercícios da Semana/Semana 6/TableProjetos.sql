a21362d91405da06708626203b2b1b36f657194a
wgeovanni <willyangeo@gmail.com>
2023-07-31 20:40:32 -0300

    Exercicio 4: Implementação da tabela Tarefa

--- Exercícios da Semana/Semana 6/TableProjetos.sql
+++ Exercícios da Semana/Semana 6/TableProjetos.sql
@@ -8,3 +8,18 @@ descricao VARCHAR(100),
 data_inicio DATE,
 data_prazo DATE
 );
+
+--Exercicio 4
+create table Tarefa(
+
+	ID serial PRIMARY KEY,
+	nome VARCHAR(100),
+	descricao VARCHAR(100),
+	data_inicio DATE,
+	data_prazo DATE,
+	concluida BOOLEAN,
+	projetoID INT,
+	categoriaID INT,
+	foreign key (projetoID) references Projeto (ID),
+	foreign key (categoriaID) references Categoria (ID) --Exercicio 8
+);
