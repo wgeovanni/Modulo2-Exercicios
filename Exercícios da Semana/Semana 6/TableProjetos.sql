553746f2db873cf8e508d0dfd3bbb830dc8319f4
wgeovanni <willyangeo@gmail.com>
2023-07-31 22:37:26 -0300

    Exercicio 9: Inserção de dados na tabela

--- Exercícios da Semana/Semana 6/TableProjetos.sql
+++ Exercícios da Semana/Semana 6/TableProjetos.sql
@@ -37,3 +37,21 @@ create table Categoria(
 	nome VARCHAR(50),
 	descricao VARCHAR(200)
 );
+
+--Exercicio 9
+insert into Projeto (nome, descricao, data_inicio, data_prazo) values
+	('Projeto A', 'Descrição do Projeto A', '2023-01-01', '2023-02-28'),
+	('Projeto B','Descrição do Projeto B','2023-03-01', '2023-04-30'),
+	('Projeto C','Descrição do Projeto B','2023-05-01', '2023-06-30');
+	
+insert into Categoria (nome, descricao) values
+	('Tarefa pessoal', 'Tarefa relacionada a assuntos pessoais.'),
+	('Tarefa profissional', 'Tarefa relacionada ao trabalho.'),
+	('Tarefa acadêmica', 'Tarefa relacionada aos estudos.');
+	
+insert into Tarefa (nome, descricao, data_inicio, data_prazo, concluida, projetoID, categoriaID) values
+	('Tarefa 1', 'Descrição da tarefa 1', '2023-01-05', '2023-01-15', true, 1, 1),
+	('Tarefa 2', 'Descrição da tarefa 2', '2023-01-10', '2023-01-20', false, 1, 2),
+	('Tarefa 3', 'Descrição da tarefa 3', '2023-03-05', '2023-03-15', false, 2, 3),
+	('Tarefa 4', 'Descrição da tarefa 4', '2023-03-10', '2023-03-20', false, 2, 1),
+	('Tarefa 5', 'Descrição da tarefa 5', '2023-05-05', '2023-05-15', false, 3, 2);
